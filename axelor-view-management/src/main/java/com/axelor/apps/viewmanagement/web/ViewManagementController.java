26c4b8fa439df3cc5a5ff39e453c5484ccb2476f
Kasym Baryktabasov <kasymk@gmail.com>
2023-10-24 19:49:34 +0600

    New Module created

---
+++ axelor-view-management/src/main/java/com/axelor/apps/viewmanagement/web/ViewManagementController.java
@@ -0,0 +1,45 @@
+package com.axelor.apps.viewmanagement.web;
+
+import com.axelor.meta.schema.views.AbstractView;
+import com.axelor.meta.service.MetaService;
+import com.axelor.rpc.Request;
+import com.axelor.rpc.Response;
+import com.google.common.collect.Maps;
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Map;
+
+@Path("/ws/viewmanagement")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class ViewManagementController{
+    private static final Logger LOG = LoggerFactory.getLogger(ViewManagementController.class);
+
+    @Inject
+    private MetaService service;
+    @POST
+    @Path("")
+    public Response getView(Request request) {
+        Map<String, Object> data = Maps.newHashMap();
+
+        final String model = request.getModel();
+        final String name = (String) request.getData().get("name");
+        final String type = (String) request.getData().get("type");
+
+        final Response response = service.findView(model, name, type);
+        final AbstractView view = (AbstractView) response.getData();
+
+        data.put("view", view);
+
+        response.setData(data);
+        response.setStatus(Response.STATUS_SUCCESS);
+        return response;
+    }
+}
